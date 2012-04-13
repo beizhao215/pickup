85858818a2a1c74b9bf4828ed941a5c5f9d798e2
Bei Zhao <bei.zhao215@gmail.com>
2012-04-13 01:05:06 -0500

    Add StaticPage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickup::Application.config.secret_token = 'cd29835907197cd6df9d17cca6751a024c4333d9aa99b8ec0b2a1cd6e3b68b05e8ef1fdf6ae110c558cde1a6a6ea4c2a0505a51721545cf4f598c15be258d8e0'
