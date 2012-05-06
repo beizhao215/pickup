620ac6c0874435f64103213aacf0cc78f8e8f9e0
Bei Zhao <bei.zhao215@gmail.com>
2012-05-05 23:23:23 -0500

    Finish all posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,17 @@
 class PostsController < ApplicationController
-  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :signed_in_user, only: [:index, :create, :destroy]
 
+  def index
+   
+    @posts = Post.all
+  end
+  
   def create
     @user = current_user
     @post = current_user.posts.build(params[:post])
     if @post.save
       flash[:success] = "Post created!"
-      redirect_to root_path
+      redirect_to posts_path
     else
       render 'users/show'
     end
